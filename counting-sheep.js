8a45df621607300af0143e6ae0a5ad4f8e5db895
Clinton Owen <clint@clintonowen.com>
2018-10-01 11:15:05 -0500

    Complete counting-sheep.js

---
+++ counting-sheep.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function countSheep (num) {
+  if (num === 0) {
+    return;
+  } else {
+    console.log(`${num} - Another sheep jumped over the jeep`);
+    countSheep(num - 1);
+  }
+}
+
+countSheep(20);
